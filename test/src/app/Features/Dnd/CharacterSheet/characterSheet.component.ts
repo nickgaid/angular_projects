d22599495c5328b4dfc6b8c7cdc5a2e7f292e6d2
Nikolaos Gkaidatzis <91544466+nickgaid@users.noreply.github.com>
2023-08-16 16:22:39 +0300

    Update characterSheet.component.ts

--- test/src/app/Features/Dnd/CharacterSheet/characterSheet.component.ts
+++ test/src/app/Features/Dnd/CharacterSheet/characterSheet.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
 @Component({
   standalone: true,
   selector: 'app-characterSheet',
   templateUrl: './characterSheet.component.html',
   styleUrls: ['./characterSheet.component.css'],
-  imports: [MatInputModule, FormsModule]
+  imports: [MatInputModule, FormsModule, MatIconModule]
 })
 export class CharacterSheetComponent implements OnInit {
 
